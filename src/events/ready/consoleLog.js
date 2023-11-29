9b801eac4cb1e169d7203eea9aa3f077f5d446ae
rivasdiego-dev <rivasdiego.dev@gmail.com>
2023-11-29 03:01:09 -0600

    :mute: Remove unnecessary console logs in ready event

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -5,20 +5,5 @@ module.exports = async client => {
 
     const server = await client.guilds.fetch(process.env.GUILD_ID);
     if (!server) return console.log(`No server found with ID: ${process.env.GUILD_ID}`);
-
-    /* try {
-        const channels = await server.channels.fetch();
-        console.log(`Found ${channels.size} channels on server ${server.name}`);
-        channels.forEach(channel => console.log(`Channel name: ${channel.name}, type: ${channel.type}, ID: ${channel.id}`));
-    } catch (error) {
-        console.error(error);
-    } */
-
-    /* try {
-        const roles = await server.roles.fetch();
-        console.log(`Found ${roles.size} roles on server ${server.name}`);
-        roles.forEach(role => console.log(`Role name: ${role.name}, ID: ${role.id}`));
-    } catch (error) {
-        console.error(error);
-    } */
+    
 };
