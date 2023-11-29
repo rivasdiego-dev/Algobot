3ad9443a6c6f62b6db4bec0308aaf69df6e7c47e
rivasdiego-dev <rivasdiego.dev@gmail.com>
2023-11-29 03:00:19 -0600

    :fire: Remove comments from index.js

--- src/index.js
+++ src/index.js
@@ -12,23 +12,6 @@ const client = new Client({
   ],
 });
 
-
 eventHandler(client);
 
-
 client.login(process.env.API_TOKEN);
-
-/* 
- bot.on("guildMemberAdd", async (member) => {
-  const defaultRole = member.guild.roles.cache.find(role => role.name === "Member");
-  if (defaultRole) {
-    member.roles.add(defaultRole);
-  }
-
-  const channel = member.guild.channels.cache.find(channel => channel.name === "general");
-  if (channel) {
-    member.voice.setChannel(channel);
-  }
-});
-
- */
